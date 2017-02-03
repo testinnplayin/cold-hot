a4ce343e03cdc5246a18caf2bcdf81615ee72689
R Wood <archaurore@gmail.com>
2017-02-03 11:34:14 -0500

    got POST command to work

--- guessesRouter.js
+++ guessesRouter.js
@@ -22,4 +22,35 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['numberOfGuesses'];
+	let lng = requiredFields.length;
+
+	console.log(req.body);
+	
+	for (let i = 0; i < lng; i++) {
+		let field = requiredFields[i];
+
+		if(!(field in req.body)) {
+			let msg = `Please specify a value for ${field}`;
+			console.error(msg);
+			return res.status(400).send(msg);
+		}
+	}
+
+	Guess
+	.create({
+		game: {
+			numberOfGuesses: req.body.numberOfGuesses
+		}
+	})
+	.then(function(guess) {
+		res.status(201).json(guess.apiRepr());
+	})
+	.catch(function(err) {
+		console.error(err);
+		res.status(500).json({ message : 'Internal server error, cannot post' });
+	});
+});
+
 module.exports = router;
