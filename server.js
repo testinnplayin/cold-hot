54bd293954580cc888929d603b8532e86ce8e255
R Wood <archaurore@gmail.com>
2017-02-03 09:15:06 -0500

    set up config for server and added Mongoose to server

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
+
+const {DATABASE_URL, PORT} = require('./config');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('common'));
 app.use(express.static('build'));
+
+mongoose.Promise = global.Promise;
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../build/index.html'));
