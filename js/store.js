e8004b1f5d31c42e2f7ce65ebe6f02feffbae488
R Wood <archaurore@gmail.com>
2017-02-03 17:20:53 -0500

    set up store file with redux thunk

--- js/store.js
+++ js/store.js
@@ -1,7 +1,8 @@
 'use strict';
 
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 
 import * as reducers from './reducers/index';
 
-export default createStore(reducers.gameReducer);
+export default createStore(reducers.gameReducer, applyMiddleware(thunk));
